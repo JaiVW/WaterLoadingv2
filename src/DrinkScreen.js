e15145dd9c6f97f9db6b4c3268267c17da7f0e31
Jai Vellala Wilson <jaivellalawilson@hotmail.co.uk>
2023-05-26 14:46:17 +0200

    some faults, package working properly

--- src/DrinkScreen.js
+++ src/DrinkScreen.js
@@ -1,13 +1,18 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Button } from 'react-native';
-import Animated from 'react-native-reanimated';
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, Button, Dimensions } from 'react-native';
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
+const { height } = Dimensions.get('window');
 
 const DrinkScreen = ({ route, navigation }) => {
   const { totalLiters, drinkingHours } = route.params;
   const [counter, setCounter] = useState(0);
   const [fillColor, setFillColor] = useState('white');
-  const [fillAnimation] = useState(new Animated.Value(0));
+  const fillAnimation = useSharedValue(0);
+
+  const config = {
+    duration: 1000,
+  };
 
   const drinkingTimePerLiter = Math.floor((parseInt(drinkingHours.split(' ')[0]) || 0) * 60 / totalLiters);
   const drinkingTimeHours = Math.floor(drinkingTimePerLiter / 60);
@@ -20,22 +25,21 @@ const DrinkScreen = ({ route, navigation }) => {
 
       setCounter(newCounter);
 
-      Animated.timing(fillAnimation, {
-        toValue: fillValue,
-        duration: 1000,
-        useNativeDriver: false,
-      }).start(() => {
-        if (newCounter >= totalLiters) {
-          setFillColor('white');
-        }
-      });
+      fillAnimation.value = withTiming(fillValue, config);
+      
+      if (newCounter >= totalLiters) {
+        setFillColor('white');
+      }
     }
   };
 
-  const fillHeight = fillAnimation.interpolate({
-    inputRange: [0, 1],
-    outputRange: ['0%', '100%'],
-    extrapolate: 'clamp',
+  const fillStyle = useAnimatedStyle(() => {
+    const animatedHeight = height * fillAnimation.value;
+
+    return {
+      height: animatedHeight,
+      backgroundColor: fillColor,
+    };
   });
 
   const goBack = () => {
@@ -54,7 +58,7 @@ const DrinkScreen = ({ route, navigation }) => {
 
   return (
     <View style={styles.container}>
-      <Animated.View style={[styles.fill, { height: fillHeight, backgroundColor: fillColor }]} />
+      <Animated.View style={[styles.fill, fillStyle]} />
       <Text style={styles.text}>Drink {totalLiters.toFixed(2)} liters per day.</Text>
       <Text style={styles.text}>{drinkingHours} hours to do this.</Text>
       <Text style={styles.text}>{drinkingTimeText}</Text>
@@ -76,9 +80,9 @@ const DrinkScreen = ({ route, navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    height: height,
     justifyContent: 'center',
     alignItems: 'center',
-    padding: 16,
     backgroundColor: 'blue',
   },
   text: {
